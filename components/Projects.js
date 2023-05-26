53f2621e8e395aee335c09556db076e6c5c5e775
arjybltrn <arjaybeltran16@gmail.com>
2023-05-25 21:59:21 -0400

    edits project desc

--- components/Projects.js
+++ components/Projects.js
@@ -12,28 +12,28 @@ import 'aos/dist/aos.css';
 const projects = [
     {
         name: '// HLLO WRLD',
-        description: 'Search a country and find basic information about it',
+        description: 'Discover basic information about any country by searching for it. Explore details such as population, languages spoken, currency, and more.',
         liveSite: 'https://hello-world-rose-sigma.vercel.app/',
         img: '/images/helloworld.png',
         technologies: ['JavaScript', 'HTML', 'CSS', 'AJAX'],
     } ,
     {
         name: 'docket.',
-        description: 'To do list',
+        description: 'Stay organized with a simple and intuitive to-do list application. Keep track of your tasks, set reminders, and easily manage your daily agenda.',
         liveSite: 'https://docket-project-two.herokuapp.com/',
         img: '/images/docket.png',
         technologies: ['JavaScript', 'HTML', 'CSS', 'Express.js', ],
     } ,
     {
         name: 'Choruso',
-        description: 'Music Application - [Group Project] ',
+        description: 'Immerse yourself in the world of music with Choruso, a collaborative music application. Discover, listen to, and share your favorite songs and playlists with friends.',
         liveSite: 'https://choruso.herokuapp.com/',
         img: '/images/choruso.png',
         technologies: ['Python', 'Django', 'Postgres'],
     } ,
     {
         name: 'Trackly',
-        description: 'Job Application Tracker',
+        description: 'Simplify your job application process with Trackly, a comprehensive job application tracker. Keep tabs on your applications, track progress, and stay organized in your job search.',
         liveSite: 'https://legendary-halva-8abb3f.netlify.app/',
         img: '/images/trackly.png',
         technologies: ['React', 'Node.js', 'MongoDB', 'Express.js'],
